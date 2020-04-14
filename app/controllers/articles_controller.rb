33364c669e63426aaa6ec9e1d5a4795ad182c970
Liliana <liliana.cholody@gmail.com>
2020-04-15 01:15:29 +0200

    error and flash desplay

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,26 @@
 class ArticlesController < ApplicationController
 
   def show
+    byebug
     @article = Article.find(params[:id])
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created succesfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 end

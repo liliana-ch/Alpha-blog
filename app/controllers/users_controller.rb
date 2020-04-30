41a91c858e0452e3fb1cb0592b02dee79cd9f979
Liliana <liliana.cholody@gmail.com>
2020-05-01 01:06:47 +0200

    Edit template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+  else
+    render 'edit'
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -14,9 +27,14 @@ class UsersController < ApplicationController
     end
   end
 
+
+
+
+
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 
 end

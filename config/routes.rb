2e73a3629d8f6d2966c22b1d5b8575ffbb4c9bb0
Liliana <liliana.cholody@gmail.com>
2020-05-14 17:51:12 +0200

    creating and testin categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+
   resources :articles
+
   get 'signup', to: 'users#new'
   resources :users, exept: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
 end

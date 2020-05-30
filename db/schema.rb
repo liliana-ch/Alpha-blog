7ad2cb37e9849f2093cb9474be4d591685d741b6
Liliana <liliana.cholody@gmail.com>
2020-05-31 00:09:16 +0200

    articles-categories associacions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_09_115716) do
+ActiveRecord::Schema.define(version: 2020_05_30_215303) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
